bd32278fc0998dd2925ac9781eb5b803dd207d17
f12o <gh@f12o.com>
2025-12-12 14:15:04 +0900

    chore: プロジェクトの依存関係をインストールし、Procfileを更新

---
+++ direct-go/version.go
@@ -0,0 +1,10 @@
+// Package direct provides version information.
+package direct
+
+const (
+	// Version is the current version of direct-go.
+	Version = "0.1.0"
+
+	// BasedOn indicates the original direct-js version this is based on.
+	BasedOn = "direct-js (L is B internal)"
+)
